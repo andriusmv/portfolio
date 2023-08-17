ad62818b893dc62a56e4fa1b34843e058910c4f4
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2023-08-17 15:11:44 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import 'nextra-theme-docs/style.css'
+import Head from 'next/head'
+import Script from 'next/script'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  <Head>
+    <title>Andrés Moreno Portfolio</title>
+        <Script
+          async
+          src="https://analytics.umami.is/script.js"
+          data-website-id="ccaaeca6-9b78-4c2f-8daf-81b9b7e58cfd"
+        />
+</Head>
+  )
 }
