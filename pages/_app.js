e42f6fff7b0cde46165f45cbc5ca0cc8016f2ba4
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2023-08-17 15:36:00 +0200

    Update _app.js missing <>

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,12 @@ import Script from 'next/script'
 
 export default function Nextra({ Component, pageProps }) {
   return (
+    <>
 <head>
     <title>Andrés Moreno Portfolio</title>
         <Script async src="https://analytics.umami.is/script.js" data-website-id="ccaaeca6-9b78-4c2f-8daf-81b9b7e58cfd" />
 </head>
     <Component {...pageProps} />
+    </>
   )
 }
